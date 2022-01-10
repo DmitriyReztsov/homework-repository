ad68a29cf480be9acf23fc2226aa5204f4d33113
DmitriyReztsov <rezcov_d@mail.ru>
2022-01-10 22:22:16 +0300

    hw11 reviewed

--- tests/homework11/test_hw1.py
+++ tests/homework11/test_hw1.py
@@ -5,6 +5,9 @@ from homework11.hw1 import SimplifiedEnum, SimplifiedEnum2
 test_data = [
     (("one", "two", "three"), {"one": "one", "two": "two", "three": "three"}),
     (("one", "two", "three", "two"), {"one": "one", "two": "two", "three": "three"}),
+]
+
+test_data_error = [
     (("one", "two", "three", "two"), {"four": AttributeError}),
 ]
 
@@ -15,11 +18,17 @@ def test_class(test_input, expected):
         __keys = test_input
 
     for key, value in expected.items():
-        if key != "four":
+        assert getattr(TetsEnums, key) == value
+
+
+@pytest.mark.parametrize("test_input, expected", test_data_error)
+def test_class_error(test_input, expected):
+    class TetsEnums(metaclass=SimplifiedEnum):
+        __keys = test_input
+
+    for key, value in expected.items():
+        with pytest.raises(AttributeError):
             assert getattr(TetsEnums, key) == value
-        else:
-            with pytest.raises(AttributeError):
-                assert getattr(TetsEnums, key) == value
 
 
 @pytest.mark.parametrize("test_input, expected", test_data)
@@ -28,8 +37,14 @@ def test_def(test_input, expected):
         __keys = test_input
 
     for key, value in expected.items():
-        if key != "four":
+        assert getattr(TetsEnums, key) == value
+
+
+@pytest.mark.parametrize("test_input, expected", test_data_error)
+def test_def_error(test_input, expected):
+    class TetsEnums(metaclass=SimplifiedEnum2):
+        __keys = test_input
+
+    for key, value in expected.items():
+        with pytest.raises(AttributeError):
             assert getattr(TetsEnums, key) == value
-        else:
-            with pytest.raises(AttributeError):
-                assert getattr(TetsEnums, key) == value
