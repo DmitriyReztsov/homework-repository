75cadd1fcd8d748a30a83ad535608bb76c388efc
DmitriyReztsov <rezcov_d@mail.ru>
2022-01-25 23:01:06 +0300

    server

--- final_task/server.py
+++ final_task/server.py
@@ -1,32 +1,45 @@
 import asyncio
 import os
 import subprocess
+from contextlib import contextmanager
 
 import websockets
+from dotenv import load_dotenv
 
-TIMEOUT = 1
+load_dotenv()
+TIMEOUT = int(os.getenv("TIMEOUT")) if os.getenv("TIMEOUT") else 10
+
+
+@contextmanager
+def file_open(path):
+    try:
+        yield
+    finally:
+        os.remove(path)
 
 
 async def execution(websocket, text):
     with open("script.py", "w", encoding="utf8") as file:
         file.write(text)
         path = os.path.abspath(file.name)
-    proc = subprocess.Popen(
-        ["python", "-u", path],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        text=True,
-        encoding="utf8",
-    )
-    for line in iter(proc.stdout.readline, ""):
-        line = line.rstrip()
-        # yield line
-        await websocket.send(line)
 
-    for line in iter(proc.stderr.readline, ""):
-        line = line.rstrip()
-        # yield line
-        await websocket.send(line)
+    with file_open(path):
+        proc = subprocess.Popen(
+            ["python", "-u", path],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+            encoding="utf8",
+        )
+        for line in iter(proc.stdout.readline, ""):
+            line = line.rstrip()
+            # yield line
+            await websocket.send(line)
+
+        for line in iter(proc.stderr.readline, ""):
+            line = line.rstrip()
+            # yield line
+            await websocket.send(line)
 
 
 async def start_server(websocket):
